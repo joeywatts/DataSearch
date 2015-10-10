aaf2f72568f877a73a410aae403c65f4b14d41dc
Joey Watts <joey.watts.96@gmail.com>
2015-10-10 11:16:04 -0400

    Initialize project.

---
+++ src/main/java/com/hacknc/MainActivity.java
@@ -0,0 +1,22 @@
+package com.hacknc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText("Hello world!");
+    }
+
+}
